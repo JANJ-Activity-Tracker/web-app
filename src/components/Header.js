8a7e4cc3447017f68e57541689fa5ea28027ab57
srnazng <srnazng@gmail.com>
2021-08-31 13:01:04 -0400

    menu to switch pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -68,7 +68,7 @@ export default function Header({ setPage }) {
                         open={menuOpen}
                         onClose={handleClose}
                     >
-                        <MenuItem onClick={handleClose}>Dashboard</MenuItem>
+                        <MenuItem onClick={() => setPage("dashboard")}>Dashboard</MenuItem>
                         <MenuItem onClick={() => setPage("profile")}>Edit Profile</MenuItem>
                         <MenuItem onClick={logout}>Log Out</MenuItem>
                     </Menu>
